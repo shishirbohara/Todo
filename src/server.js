152a88da774526092eec94103f6fc05b9bf2f78d
shishirbohara <shishirbohara68@gmail.com>
2025-01-12 11:33:34 +0545

    initialize SQLite schema with users and todos tables

--- src/server.js
+++ src/server.js
@@ -3,11 +3,12 @@ import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
